bec90d9e1432a107c872bc444cd3bca3f8a061d1
Alan <alankash9@gmail.com>
2022-03-10 14:54:54 -0800

    React app with map

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Map from './components/Map'
+
+function App() {
+  return (
+    <div className="App">
+      Hello World.
+      <Map/>
+    </div>
+  );
+}
+
+export default App;
