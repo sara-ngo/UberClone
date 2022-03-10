544e9e4824441c02a9d777004b51aef10556da53
Alan <alankash9@gmail.com>
2022-03-10 14:59:41 -0800

    Node server connects to MongoDB

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import User from './models/user.js'
+// Required environment variables- MONGO_URI
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+// connect to database
+mongoose.connect(process.env.MONGO_URI)
+  .then((result) => {
+    result.connection.client.db().admin().listDatabases()
+      .then((result) => {
+        result.databases.forEach((db) => console.log(` - ${db.name}`))
+      })
+    })
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"))
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT || 5000, function () {
+  console.log("Node is running at http://localhost:" + listener.address().port)
+})
+
+// get something from database
+app.get('/', (req, res) => {
+  User.User.find({}, (err, data) => {
+    res.send(data)
+  })
+})
+
+export default app
