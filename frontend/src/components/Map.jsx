eb7179fac863d20f1ec94f92ad60c8e98683c760
Sara Ngo <sara.ngoo.9641@gmail.com>
2022-03-16 20:44:28 -0700

    -add direction to mapbox
    -The map is able to navigate the shortest route between two points now. The default point will be hard-coded SJSU, the 2nd one will be up to the user's selection.

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -6,62 +6,188 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // for zoom and navigation control
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
 
 const Map = () => {
-  const mapContainerRef = useRef(null);
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-121.881073);
+  const [lat, setLat] = useState(37.335186);
+  const [zoom, setZoom] = useState(12);
+  const start = [lng, lat];
 
-  const [lng, setLng] = useState(-121.88);
-  const [lat, setLat] = useState(37.33);
-  const [zoom, setZoom] = useState(13);
-
-  // Initialize map when component mounts
   useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [lng, lat],
       zoom: zoom
     });
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+    route();
 
-    // Add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl());
+  }, [map.current]);
 
-    // Add geolocate control to the map.
-    map.addControl(
+  const locate = () => {
+    map.current.addControl(
       new mapboxgl.GeolocateControl({
-      positionOptions: {
-      enableHighAccuracy: true
-      },
-      // When active the map will receive updates to the device's location as it changes.
-      trackUserLocation: true,
-      // Draw an arrow next to the location dot to indicate which direction the device is heading.
-      showUserHeading: true
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        style: {
+          right: 10,
+          top: 10
+        },
+        position: 'bottom-left',
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
       })
     );
+  }
 
-    // Add navigator directions
+  const route = () => {
+    locate();
+    map.current.on('load', () => {
+      // make an initial directions request that
+      // starts and ends at the same location
+      // getRoute(start);
 
+      // Add starting point to the map
+      map.current.addLayer({
+        id: 'point',
+        type: 'circle',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: [
+              {
+                type: 'Feature',
+                properties: {},
+                geometry: {
+                  type: 'Point',
+                  coordinates: start
+                }
+              }
+            ]
+          }
+        },
+        paint: {
+          'circle-radius': 10,
+          'circle-color': '#3887be'
+        }
+      });
 
-    
-
-    map.on('move', () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
+      map.current.on('click', (event) => {
+        const coords = Object.keys(event.lngLat).map((key) => event.lngLat[key]);
+        const end = {
+          type: 'FeatureCollection',
+          features: [
+            {
+              type: 'Feature',
+              properties: {},
+              geometry: {
+                type: 'Point',
+                coordinates: coords
+              }
+            }
+          ]
+        };
+        if (map.current.getLayer('end')) {
+          map.current.getSource('end').setData(end);
+        } else {
+          map.current.addLayer({
+            id: 'end',
+            type: 'circle',
+            source: {
+              type: 'geojson',
+              data: {
+                type: 'FeatureCollection',
+                features: [
+                  {
+                    type: 'Feature',
+                    properties: {},
+                    geometry: {
+                      type: 'Point',
+                      coordinates: coords
+                    }
+                  }
+                ]
+              }
+            },
+            paint: {
+              'circle-radius': 10,
+              'circle-color': '#f30'
+            }
+          });
+        }
+        getRoute(coords);
+      });
     });
+  }
 
-    // Clean up on unmount
-    return () => map.remove();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  async function getRoute(end) {
+    const query = await fetch(
+      `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+      { method: 'GET' }
+    );
+    const json = await query.json();
+    const data = json.routes[0];
+    const route = data.geometry.coordinates;
+    const geojson = {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'LineString',
+        coordinates: route
+      }
+    };
+    // if the route already exists on the map, we'll reset it using setData
+    if (map.current.getSource('route')) {
+      map.current.getSource('route').setData(geojson);
+    }
+    // otherwise, we'll make a new request
+    else {
+      map.current.addLayer({
+        id: 'route',
+        type: 'line',
+        source: {
+          type: 'geojson',
+          data: geojson
+        },
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#3887be',
+          'line-width': 5,
+          'line-opacity': 0.75
+        }
+      });
+    }
+    // get the sidebar and add the instructions
+    const instructions = document.getElementById('instructions');
+    const steps = data.legs[0].steps;
+
+    let tripInstructions = '';
+    for (const step of steps) {
+      tripInstructions += `<li>${step.maneuver.instruction}</li>`;
+    }
+    instructions.innerHTML = `<p><strong>Trip duration: ${Math.floor(
+      data.duration / 60
+    )} min 🚴 </strong></p><ol>${tripInstructions}</ol>`;
+  }
 
   return (
-    <div>
-      <div class="sidebar">
-        <div class="heading">
-          <h1>Routes</h1>
-        </div>
-        <div id="reports" class="reports"></div>
-    </div>
-      <div className='map-container' ref={mapContainerRef} />
-    </div>
+    <>
+      <div ref={mapContainer} className="map-container" />
+      <div id="instructions" className="instructions"></div>
+    </>
   );
 };
 
