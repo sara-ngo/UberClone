85a7a42aa1a5e6eda65c5b559a3bc49c072ab0c7
Sara Ngo <sara.ngoo.9641@gmail.com>
2022-03-31 20:26:25 -0700

    add costEstimation
    -costEstimation will calculate the fare base on trip duration and display it on Rider view
    -navigation will be used at driver site

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -5,100 +5,66 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '../../styles/Map.css';
 import 'mapbox-gl/dist/mapbox-gl.css'; // for zoom and navigation control
 import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import calculateRoute from './costEstimation';
 
-import getRoute from './Navigation';
-
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+const ACCESS_TOKEN = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+mapboxgl.accessToken = ACCESS_TOKEN;
 
 const Map = () => {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  
-  var [lng, setLng] = useState(-121.881073);
-  var [lat, setLat] = useState(37.335186);
+  const [lng, setLng] = useState(-121.881073);
+  const [lat, setLat] = useState(37.335186);
   const [zoom, setZoom] = useState(12);
   
-  // starting point
-  const start = [lng, lat];
+  var start = [lng,lat];
 
   // search address box + marker 
   const geocoder = new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
     marker: {
-    color: 'orange'
+    color: 'blue'
     },
     mapboxgl: mapboxgl
-    });
+  });
+  const addGeoCoder = () => {
+    map.current.addControl(geocoder);
+  }
 
-  // render the map after the side load
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-
-    // let lat = geolocationCoordinatesInstance.latitude;
-    // let lng = geolocationCoordinatesInstance.longitude;
-    // console.log("LAT =" + lat);
-    // console.log("LONG =" + lng);
-    
-    // navigator.geolocation.getCurrentPosition(function(position) {
-    //     console.log("Latitude is :", position.coords.latitude);
-    //     console.log("Longitude is :", position.coords.longitude);
-    // });
-
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      //starting point is center of the map
-      center: [lng, lat],
-      zoom: zoom
-    });
-
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-
-    addGeoCoder(); // get user input
-    route(); // generate route
-    zoomControl();
-
-  }, [map.current]);
-
+  // zoom controller
   const zoomControl = () => {
     map.current.addControl(
       new mapboxgl.NavigationControl(), "top-left"
     );
   }
 
-  const addGeoCoder = () => {
-    map.current.addControl(geocoder);
+  // get user's real time location
+  const geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true,
+    },
+    // When active the map will receive updates to the device's location as it changes.
+    trackUserLocation: true,
+    style: {
+      right: 10,
+      top: 10
+    },
+    // Draw an arrow next to the location dot to indicate which direction the device is heading.
+    showUserHeading: true
+  });
+  const addGeolocate = () => {
+    map.current.addControl(geolocate, "top-right");
   }
 
-  // get real time location
-  const geolocate = () => {
-    map.current.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
-        // When active the map will receive updates to the device's location as it changes.
-        trackUserLocation: true,
-        style: {
-          right: 10,
-          top: 10
-        },
-        position: 'bottom-left',
-        // Draw an arrow next to the location dot to indicate which direction the device is heading.
-        showUserHeading: true
-      })
-    );
-  }
-
+  useEffect(() => {
+    geolocate.on('geolocate', function(position) {
+      console.log('geolocateeee');
+      console.log(position.coords.latitude);
+    });
+  });
+  
+  // get direction
   const route = () => {
-    geolocate();
-
-    console.log("helo");
-
     map.current.on('load', () => {
       // make an initial directions request that
       // starts and ends at the same location
@@ -173,16 +139,51 @@ const Map = () => {
             }
           });
         }
-        getRoute(coords, start, map);
+        calculateRoute(coords, start, map);
       });
     });
   }
 
+    // render the map after the side load
+    useEffect(() => {
+      if (map.current) return; // initialize map only once
+  
+      // let lat = geolocationCoordinatesInstance.latitude;
+      // let lng = geolocationCoordinatesInstance.longitude;
+      // console.log("LAT =" + lat);
+      // console.log("LONG =" + lng);
+      
+      // navigator.geolocation.getCurrentPosition(function(position) {
+      //     console.log("Latitude is :", position.coords.latitude);
+      //     console.log("Longitude is :", position.coords.longitude);
+      // });
+  
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        //starting point is center of the map
+        center: [lng, lat],
+        zoom: zoom
+      });
+      
+      map.current.on('move', () => {
+        setLng(map.current.getCenter().lng.toFixed(4));
+        setLat(map.current.getCenter().lat.toFixed(4));
+        setZoom(map.current.getZoom().toFixed(2));
+      });
 
+      zoomControl();
+      addGeoCoder(); // get user input
+      addGeolocate(); //get current location
+      route(); // generate route
+      calculateRoute();
+    });
+
+    
   return (
     <>
       <div ref={mapContainer} className="map-container" />
-      <div id="instructions" className="instructions"></div>
+      <div id="costEst" className="costEst"></div>
     </>
   );
 };
@@ -191,5 +192,4 @@ export default Map;
 
 
 //TODO:
-//show marker when locate an address
 //identify start point and endpoint
---
+++ frontend/src/templates/Driver.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/App.css'
+import Map from '../components/Map/Map'
+
+function Driver() {
+  return (
+    <div>
+        <p>Driver - from Driver.js</p>
+        <Map/>
+    </div>
+  );
+}
+
+export default Driver;
