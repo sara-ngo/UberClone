92178813e447cb933e9491076e278d128291f438
bradosia <32529373+bradosia@users.noreply.github.com>
2022-04-07 07:48:28 -0700

    geolocate on map load

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -8,9 +8,10 @@ import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import calculateRoute from './calculateRoute';
 import getRoute from './Navigation';
 
-
 const ACCESS_TOKEN = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
 mapboxgl.accessToken = ACCESS_TOKEN;
+
+var start = [-122.405818, 37.802374];
 
 const Map = (props) => {
   const mapContainer = useRef(null);
@@ -19,13 +20,11 @@ const Map = (props) => {
   const [lat, setLat] = useState(37.802374);
   const [zoom, setZoom] = useState(12);
 
-  var start = [lng,lat];
-
   // search address box + marker
   const geocoder = new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
     marker: {
-    color: 'blue'
+      color: 'blue'
     },
     mapboxgl: mapboxgl
   });
@@ -33,17 +32,15 @@ const Map = (props) => {
     map.current.addControl(geocoder);
   }
 
-
   // zoom controller
   const zoomControl = () => {
     map.current.addControl(new mapboxgl.NavigationControl(), "top-left");
   }
 
-
   // get user's real time location
   const geolocate = new mapboxgl.GeolocateControl({
     positionOptions: {
-      enableHighAccuracy: true,
+      enableHighAccuracy: true
     },
     // When active the map will receive updates to the device's location as it changes.
     trackUserLocation: true,
@@ -58,15 +55,14 @@ const Map = (props) => {
     map.current.addControl(geolocate, "top-right");
   }
 
-
-
   useEffect(() => {
     geolocate.on('geolocate', function(position) {
-      console.log('geolocateeee');
+      console.log("geolocate, " + position.coords.longitude + ", " + position.coords.latitude);
       console.log(position);
 
       setLng(position.coords.longitude);
       setLat(position.coords.latitude);
+      start = [position.coords.longitude, position.coords.latitude];
     });
   });
 
@@ -152,42 +148,54 @@ const Map = (props) => {
     });
   }
 
-    // render the map after the side load
-    useEffect(() => {
-      if (map.current) return; // initialize map only once
+  // render the map after the side load
+  useEffect(() => {
+    if (map.current)
+      return; // initialize map only once
 
-      // let lat = geolocationCoordinatesInstance.latitude;
-      // let lng = geolocationCoordinatesInstance.longitude;
-      // console.log("LAT =" + lat);
-      // console.log("LONG =" + lng);
+    // let lat = geolocationCoordinatesInstance.latitude;
+    // let lng = geolocationCoordinatesInstance.longitude;
+    // console.log("LAT =" + lat);
+    // console.log("LONG =" + lng);
 
-      // navigator.geolocation.getCurrentPosition(function(position) {
-      //     console.log("Latitude is :", position.coords.latitude);
-      //     console.log("Longitude is :", position.coords.longitude);
-      // });
+    // navigator.geolocation.getCurrentPosition(function(position) {
+    //     console.log("Latitude is :", position.coords.latitude);
+    //     console.log("Longitude is :", position.coords.longitude);
+    // });
 
-      map.current = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: 'mapbox://styles/mapbox/streets-v11',
-        //starting point is center of the map
-        center: [lng, lat],
-        zoom: zoom
-      });
-
-      map.current.on('move', () => {
-        setLng(map.current.getCenter().lng.toFixed(4));
-        setLat(map.current.getCenter().lat.toFixed(4));
-        setZoom(map.current.getZoom().toFixed(2));
-      });
-
-      zoomControl();
-      addGeoCoder(); // get user input
-      addGeolocate(); //get current location
-      route(); // generate route
-      calculateRoute();
-      getRoute();
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current, style: 'mapbox://styles/mapbox/streets-v11',
+      //starting point is center of the map
+      center: [
+        lng, lat
+      ],
+      zoom: zoom
     });
 
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+
+    /* Once we've got a position, zoom and center the map on it
+      */
+    map.current.on('locationfound', function(e) {
+      //map.fitBounds(e.bounds);
+      console.log("locationfound, " + e.latlng.lng + ", " + e.latlng.lat);
+    });
+
+    map.current.on('load', function() {
+      geolocate.trigger();
+    });
+
+    zoomControl();
+    addGeoCoder(); // get user input
+    addGeolocate(); //get current location
+    route(); // generate route
+    calculateRoute();
+    getRoute();
+  });
 
   return (
     <>
