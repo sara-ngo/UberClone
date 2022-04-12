72a2daf6215346a4d304982f73deb1323f5a60f1
Ngan Nguyen <ngannpt9@gmail.com>
2022-04-12 10:46:17 -0700

    Added profile link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { Route, Routes, Navigate } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import Profile from "./components/Profile";
 import './styles/App.css'
 
-import Home from './templates/Home'
+import Home from './templates/Home';
 import Rider from './templates/Rider'
 import Driver from './templates/Driver'
 
@@ -12,11 +13,12 @@ function App() {
   const user = localStorage.getItem("token");
   return (
     <Routes>
-         {user && <Route path="/" exact element={<Rider />} />}
+      {user && <Route path="/" exact element={<Rider />} />}
 			  <Route path="/signup" exact element={<Signup />} />
 			  <Route path="/login" exact element={<Login />} />
 			  <Route path="/" element={<Navigate replace to="/login" />} />
         <Route exact path='/' element={<Home/>} />
+        <Route exact path='/Profile' element={<Profile/>} />
         <Route exact path='/Rider' element={<Rider/>} />
         <Route exact path='/Driver' element={<Driver/>} />
     </Routes>
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -28,6 +28,11 @@ function Navbar() {
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
+          <li className="nav-item active">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
             <li className="nav-item active">
               <Link to="/profile" className="nav-link">
                 Profile
