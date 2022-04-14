3bdb91a2d1283439c887d652053a7870eb21403e
bradosia <32529373+bradosia@users.noreply.github.com>
2022-04-13 22:44:47 -0700

    server issue merging

--- server.js
+++ server.js
@@ -21,41 +21,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const httpServer = http.createServer(app)
-const io = new Server(httpServer, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  }
-})
-
-io.on('connection', socket => {
-  console.log(`User Connected: ${socket.id}`)
-
-  socket.on('request_target', () => {
-    io.allSockets().then((result) => {
-      for (let item of result) {
-        if (item != socket.id) {
-          console.log('targets:', socket.id, item)
-          socket.emit('receive_target', item)
-          socket.to(item).emit('receive_target', socket.id)
-        }
-      }
-    })
-  })
-
-  socket.on('send_pm', (data) => {
-    console.log(data)
-    socket.to(data.target).emit('receive_pm', data)
-  })
-})
-
-httpServer.listen(4000, function() {
-  console.log('Socket server listening at http://localhost:4000')
-})
-
-MapServer();
-
 // connect to database
 mongoose.connect(process.env.MONGO_URI)
   .then((result) => {
@@ -69,10 +34,8 @@ AuthServer(app);
 ChatServer(app);
 MapServer(app);
 
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"))
-
 
 // get something from database
 app.get('/', (req, res) => {
@@ -85,6 +48,5 @@ app.get('/', (req, res) => {
 const listener = app.listen(process.env.PORT || 5000, function() {
   console.log("Node is running at http://localhost:" + listener.address().port)
 })
-
 
 export default app
