c3d6eab667d0870eadb8614f2723887663ee898d
bradosia <32529373+bradosia@users.noreply.github.com>
2022-04-15 17:56:58 -0700

    cors policy

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,3 @@
 export const AUTHENTICATION_SERVER = "http://localhost:5000";
 export const CHAT_SERVER = "http://localhost:4000";
-export const MAP_SERVER = "http://localhost:4001";
+export const MAP_SERVER = "https://sjsu-cs-160-uber-clone-glitch-test.glitch.me:4001";
