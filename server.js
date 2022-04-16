d46c1677086bc98f267263b2c3bb82ef8b7ff385
bradosia <32529373+bradosia@users.noreply.github.com>
2022-04-15 18:12:27 -0700

    cors

--- server.js
+++ server.js
@@ -21,6 +21,12 @@ dotenv.config()
 
 const app = express()
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const corsOptions = {
   origin: "*",
   optionsSuccessStatus: 200
