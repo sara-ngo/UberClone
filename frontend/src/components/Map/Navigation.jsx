a8b0ac715298207f60c7e4b31c3beca226de2009
Sara Ngo <sara.ngoo.9641@gmail.com>
2022-04-17 15:09:18 -0700

    add other riders and drivers
    -loadDriverLocation: contains other driver location (hard code)
    -loadRiderLocation: contains other rider location (hard code)
    -Map.jsx: the map now will show other riders location on driver side and vice versa

--- frontend/src/components/Map/Navigation.jsx
+++ frontend/src/components/Map/Navigation.jsx
@@ -1,58 +1,59 @@
-import mapboxgl from 'mapbox-gl';
-
-var instructionEles = [];
+import mapboxgl from "mapbox-gl";
 
 async function getRoute(end, start, map) {
   console.log("getRoute");
-  const query = await fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`, {method: 'GET'});
+  const query = await fetch(
+    `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+    { method: "GET" }
+  );
   const json = await query.json();
   const data = json.routes[0];
   const route = data.geometry.coordinates;
   const geojson = {
-    type: 'Feature',
+    type: "Feature",
     properties: {},
     geometry: {
-      type: 'LineString',
-      coordinates: route
-    }
+      type: "LineString",
+      coordinates: route,
+    },
   };
   // if the route already exists on the map, we'll reset it using setData
-  if (map.current.getSource('route')) {
-    map.current.getSource('route').setData(geojson);
+  if (map.current.getSource("route")) {
+    map.current.getSource("route").setData(geojson);
   } else {
     // otherwise, we'll make a new request
-      map.current.addLayer({
-        id: 'route',
-        type: 'line',
-        source: {
-          type: 'geojson',
-          data: geojson
-        },
-        layout: {
-          'line-join': 'round',
-          'line-cap': 'round'
-        },
-        paint: {
-          'line-color': '#3887be',
-          'line-width': 5,
-          'line-opacity': 0.75
-        }
-      });
-    }
+    map.current.addLayer({
+      id: "route",
+      type: "line",
+      source: {
+        type: "geojson",
+        data: geojson,
+      },
+      layout: {
+        "line-join": "round",
+        "line-cap": "round",
+      },
+      paint: {
+        "line-color": "#3887be",
+        "line-width": 5,
+        "line-opacity": 0.75,
+      },
+    });
 
-    // get the sidebar and add the instructions
-    const instructions = document.getElementById('instructions');
-    const steps = data.legs[0].steps;
+  }
 
-    let tripInstructions = '';
-    var tripDuration = Math.floor(data.duration / 60);
+  // get the sidebar and add the instructions
+  const instructions = document.getElementById("instructions");
+  const steps = data.legs[0].steps;
 
-    for (const step of steps) {
-      tripInstructions += `<li>${step.maneuver.instruction}</li>`;
-    }
+  let tripInstructions = "";
+  var tripDuration = Math.floor(data.duration / 60);
 
-    instructions.innerHTML =
-    `<div>
+  for (const step of steps) {
+    tripInstructions += `<li>${step.maneuver.instruction}</li>`;
+  }
+
+  instructions.innerHTML = `<div>
       <p><strong>Trip duration: ${tripDuration} minutes 🚴</strong></p>
       <p><strong>Driving instructions:</strong></p>
       <ol>${tripInstructions}</ol>
@@ -60,5 +61,3 @@ async function getRoute(end, start, map) {
 }
 
 export default getRoute;
-
-
