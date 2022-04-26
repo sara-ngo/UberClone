f7f15b0f71fbc02fb3842a0e6652a0b2a1f46000
Ngan Nguyen <ngannpt9@gmail.com>
2022-04-26 16:52:52 -0700

    updated profile

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,10 +1,12 @@
 import styles from "./styles.module.css";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import TripList from "./TripList";
 
 const Profile = () => {
 	const [user, setUser] = useState(null);
+	const [profilePic, setProfilePic] = useState(null);
+	const [error, setError] = useState(false);
 
 	useEffect(() => {
 		const url = "http://localhost:5000/api/userInfo";
@@ -13,11 +15,25 @@ const Profile = () => {
 		})
 
 	}, [])
+
 	const handleLogout = () => {
 		localStorage.removeItem("token");
 		window.location.reload();
 	};
-
+    const imageHandler = (e) => {
+	   setError(false);
+       const selected = e.target.files[0];
+       const ALLOWED_TYPES = ["image/png", "image/jpeg", "image/jpg"];
+       if (selected && ALLOWED_TYPES.includes(selected.type)) {
+          const reader = new FileReader();
+          reader.onload = () => {
+             setProfilePic(reader.result);
+         };
+       reader.readAsDataURL(selected);
+      } else {
+      setError(true);
+    }
+  };
 
 	return (
 		<div className={styles.main_container}>
@@ -27,9 +43,33 @@ const Profile = () => {
 					Logout
 				</button>
 			</nav>
+			<div className={styles.container}>
+			<h5>Profile</h5>
+        {error && <p className={styles.errorMsg}>File not supported</p>}
+        <div
+          className="profilePic"
+          style={{
+            background: styles.profilePic
+              ? `url("${styles.profilePic}") no-repeat center/cover`
+              : "#131313"
+          }}
+        >
+          {!profilePic && (
+            <>
+              <p>Add an image</p>
+              <label htmlFor="fileUpload" className="customFileUpload">
+                Choose file
+              </label>
+              <input type="file" id="fileUpload" onChange={imageHandler} />
+              <span>(jpg, jpeg or png)</span>
+            </>
+          )}
+        </div>
+        {profilePic && (
+          <button onClick={() => setProfilePic(null)}>Remove image</button>
+        )}
 			{(user == null) ? '' :
 				<div>
-					<h5>Profile</h5>
 					<p>Name: {user.firstName} {user.lastName}</p>
 					<p>Email: {user.email}</p>
 					<TripList user={user} trips={user.trips} />
@@ -46,6 +86,7 @@ const Profile = () => {
 			<div>
 			<input type="submit" value="Update Profile"></input>
 			</div>
+		</div>
 		</div>
 	);
 };
