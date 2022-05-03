6bd2675d4ce397b4597398ffe2e242f701c9362b
bradosia <32529373+bradosia@users.noreply.github.com>
2022-05-03 00:04:33 -0700

    add driver and rider AI

---
+++ backend/components/MapAI/RiderAI.js
@@ -0,0 +1,78 @@
+import io from "socket.io-client";
+import * as Constants from "../../../constants.js"
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+class App {
+  constructor(lat_, long_) {
+    this.lat = lat_;
+    this.long = long_;
+    this.type = "rider";
+    this.abort = false;
+    this.userMap = new Map();
+    this.driverSocketId = "";
+    this.inProgressTogether = false;
+  }
+  async start() {
+    this.socket = io(Constants.MAP_SERVER, {
+      cors: {
+        origin: "*",
+        methods: ["PUT", "GET", "POST", "DELETE", "OPTIONS"],
+        credentials: false
+      }
+    });
+    this.positionUpdate();
+    // listen to server broadcasts
+    this.socket.on('requestRideProgress', this.requestRideProgress.bind(this));
+    this.socket.on('positionData', this.positionData.bind(this));
+    this.socket.on('tripTogetherBegin', this.tripTogetherBegin.bind(this));
+    // wait before requesting a ride
+    await sleep(3000);
+    this.socket.emit("requestRide", {
+      "rideType": "UberX",
+      "rideCost": 20,
+      "destLat" : 37.3335675106991,
+      "destLong" : -121.88175427152902
+    });
+  }
+  async positionUpdate(){
+    while(this.abort === false){
+      this.socket.emit("positionUpdate", {
+        "long" : this.long,
+        "lat" : this.lat,
+        "type" : this.type,
+        "token" : 0
+      });
+      await sleep(8000);
+    }
+  }
+
+  requestRideProgress(data){
+    //console.log("requestRideProgress Data Received:");
+    //console.log(data);
+  }
+
+  positionData(data){
+    //console.log("positionData Data Received:");
+    //console.log(data);
+    this.userMap.set(data.socketId, data);
+    // follow the driver around
+    if(this.inProgressTogether && data.socketId == this.driverSocketId){
+      this.lat = data.lat;
+      this.long = data.long;
+    }
+  }
+
+  tripTogetherBegin(data){
+    this.inProgressTogether = true;
+    this.driverSocketId = data.driverSocketId;
+    //console.log("tripTogetherBegin Data Received:");
+    //console.log(data);
+  }
+}
+
+export default App;
