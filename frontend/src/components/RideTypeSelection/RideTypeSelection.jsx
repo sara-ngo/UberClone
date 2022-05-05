0c2f82c6b5cec17cde79d992544e6a5cfc10d4f4
SLiu2022 <shaoyuelll@gmail.com>
2022-05-04 20:36:00 -0700

    Added car icons in select rides

--- frontend/src/components/RideTypeSelection/RideTypeSelection.jsx
+++ frontend/src/components/RideTypeSelection/RideTypeSelection.jsx
@@ -8,6 +8,9 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import "../../styles/CostEstimation.css";
+import car1 from '../../images/car1.png'
+import car2 from '../../images/car2.png'
+import car3 from '../../images/car3.png'
 
 const BASE_FEE = 2.0;
 const BOOKING_FEE = 2.5;
@@ -120,10 +123,12 @@ class App extends Component {
                     control={<Radio />}
                     label={this.state.uberX_Text}
                   />
+                  <img src={car1} alt="car1" 
+                  style={{width: 250, top: this.props.top, left: this.props.left}}/>
                 </CardContent>
               </CardActionArea>
             </Card>
-
+            
             <Card value="confort" onClick={chooseComfort}>
               <CardActionArea>
                 <CardContent>
@@ -132,6 +137,8 @@ class App extends Component {
                     control={<Radio />}
                     label={this.state.comfort_Text}
                   />
+                  <img src={car3} alt="car3" 
+                  style={{width: 250, top: this.props.top, left: this.props.left}}/>
                   <p id="caption">Newer cars with extra legroom</p>
                 </CardContent>
               </CardActionArea>
@@ -145,6 +152,8 @@ class App extends Component {
                     control={<Radio />}
                     label={this.state.pool_Text}
                   />
+                  <img src={car2} alt="car2" 
+                  style={{width: 250, top: this.props.top, left: this.props.left}}/>
                   <p id="caption">Share the ride with 1 to 3 people</p>
                 </CardContent>
               </CardActionArea>
